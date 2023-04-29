1c8a5040edea764c1129a4dd88b31fef304566a3
masif2002 <masif2002@outlook.com>
2023-04-29 06:52:42 +0530

    Auth Guard

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private afAuth: AngularFireAuth, private snack: SnackService) {}
+
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean> {
+    
+      
+      const user = await this.afAuth.currentUser
+      const isLoggedIn = !!user
+
+      if (!isLoggedIn) {
+        this.snack.authError()
+      }
+
+      return isLoggedIn
+
+  }
+  
+}
